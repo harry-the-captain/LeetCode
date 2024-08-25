879de70cea760fca716bd1e182234cd9b4781298
harry-the-captain <155612183+harry-the-captain@users.noreply.github.com>
2024-08-25 23:15:48 +0530

    Leetcode Q.283

---
+++ Move_zeros.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int left = 0;       // By two pointer approach
+
+        for(int right=0; right<nums.length; right++)
+        {
+            if(nums[right] != 0)
+            {
+                int temp = nums[right];
+                nums [right] = nums[left];
+                nums[left] = temp;
+                left++;
+
+            }
+        }
+    }
+}
